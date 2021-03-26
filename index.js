a108598f07492d37bac4d89b7726ab76d1874d2b
Drew M. Johnson <drew@drewmjohnson.com>
2021-03-26 21:29:25 +0000

    Added !ThreadDay command

--- index.js
+++ index.js
@@ -4,23 +4,39 @@
 // Permission to use granted under MIT License
 // Copyright (c) 2017 - 2020 Sanctuary
 
+// Initialize connection to Redis Cloud
+const redis = require('redis');
+const redis_client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+
+redis_client.on("error", function(error) {
+  console.error(error);
+});
+
 // Initialize Discord & a client
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const discord_client = new Discord.Client();
 
 // When the client is ready, run this code
 // This event will only trigger one time after logging in
-client.once('ready', () => {
+discord_client.once('ready', () => {
   console.log('Ready!');
 });
 
 // Listen for messages
-client.on('message', message => {
+discord_client.on('message', message => {
+  // Early exit for non-prefix or bot-authored commands
+  if (!message.content.startsWith('!') || message.author.bot) return;
+  
   if (message.content === '!Thor') {
 	  // send back "God of Thunder" to the channel the message was sent in
 	  message.channel.send('God of Thunder');
   }
+  if (message.content === '!ThreadDay') {
+    redis_client.get("superthread_day", function(err, reply) {
+      message.channel.send(reply);
+    });
+  }
 });
 
 // login to Discord with your app's token
-client.login(process.env.TOKEN);
+discord_client.login(process.env.TOKEN);
