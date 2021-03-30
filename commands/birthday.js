0f9d2ec729e1fcbb96d2a4efb5560b50efb6bdb8
Drew M. Johnson <drew@drewmjohnson.com>
2021-03-30 00:24:28 +0000

    Reworking birthday command

--- commands/birthday.js
+++ commands/birthday.js
@@ -3,24 +3,13 @@ module.exports = {
   description: 'Prints the birthday of the tagged user',
   execute(message, args, database) {
     const user_id = message.mentions.users.first().id;
-    console.log(`ID=${user_id}`);
-    //var user = parseUser(taggedUser.id, database);
-    var user_name;
-    database.get(user_id, function(err, reply) {
-      console.log(`reply=${reply}`);
-      user_name = reply;
-    });
-    console.log(`user=${user_name}`);
-    database.get(`${user_name}_bday`, (err, reply) => {
-      if (!err)
-        message.channel.send(reply);
+    database.get(user_id, function(err1, reply1) {
+      if (!err1) {
+        database.get(`${reply1}_bday`, (err2, reply2) => {
+          if (!err2)
+            message.channel.send(reply2);
+        });
+      }
     });
   },
 };
-
-// function parseUser(user_id, database) {
-//   database.get(user_id, function(err, reply) {
-//     console.log(`reply=${reply}`);
-//     return reply;
-//   });
-// }
